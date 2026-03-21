77e128879cba6e709417dd50490792bcc482da9f
Gaurav123-del <gauravkumarraju1@gmail.com>
2026-03-21 21:15:45 +0530

    Traingle Alphabet and Numbers  Pattern

--- src/patterns/triangleABC.java
+++ src/patterns/triangleABC.java
@@ -1,4 +1,17 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class triangleABC {
+    public static void main(String[] args) {
+        Scanner pat = new Scanner(System.in);
+        System.out.print("Enter Number of row or col = ");
+        int n = pat.nextInt();
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=i;j++){
+                System.out.print( (char)(j+64) +"  ");  // If ROW is same then print "i" and if COlUMN is same print "j"
+            }
+            System.out.println();
+        }
+    }
 }
--- src/patterns/triangleNum.java
+++ src/patterns/triangleNum.java
@@ -8,7 +8,7 @@ public class triangleNum {
         System.out.print("Enter Number of row or col = ");
         int n = pat.nextInt();
         for (int i =1;i<=n;i++){
-            for (int j =1;j<=i;j++){
+            for (int j =1;j<=i;j++){   /// if print triangle then condition is  < j<= i >.
                 System.out.print(j +"  ");  // If ROW is same then print "i" and if COlUMN is same print "j"
             }
             System.out.println();
