31fc5ca3b01e6242b472fba0b9d94954979d1603
Gaurav123-del <gauravkumarraju1@gmail.com>
2026-03-21 20:10:19 +0530

    Square ABCD  Pattern

---
+++ src/patterns/squareABC.java
@@ -0,0 +1,17 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class squareABC {
+    public static void main(String[] args) {
+        Scanner pat = new Scanner(System.in);
+        System.out.print("Enter Number of row or col = ");
+        int n = pat.nextInt();
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=n;j++){
+                System.out.print((char)(j+64) +"  ");
+            }
+            System.out.println();
+        }
+    }
+}
