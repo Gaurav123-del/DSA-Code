d269375821b9d08534e4dd69876ef4d3d40834e3
Gaurav123-del <gauravkumarraju1@gmail.com>
2026-03-21 23:57:11 +0530

    Corss STAR  Pattern

---
+++ src/patterns/multipleStar.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class multipleStar {
+    public static void main(String[] args) {
+        Scanner pat = new Scanner(System.in);
+        System.out.print("Enter Number of row or col = ");
+        int n = pat.nextInt();
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=n;j++){
+                if (i==j  || i + j == n + 1)
+                    System.out.print("*"+ "  ");
+
+                else
+                    System.out.print(" "+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
