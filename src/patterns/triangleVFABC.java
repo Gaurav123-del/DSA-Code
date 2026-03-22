56217e96f2bb905799db13bcf0c5ef26c6bbf890
Gaurav123-del <gauravkumarraju1@gmail.com>
2026-03-22 17:00:15 +0530

    Vertically Flip  triangle Alphabet  Pattern

---
+++ src/patterns/triangleVFABC.java
@@ -0,0 +1,21 @@
+//STANDARD METHODS .
+package patterns;
+
+import java.util.Scanner;
+
+public class triangleVFABC {
+    public static void main(String[] args) {
+        Scanner pat = new Scanner(System.in);
+        System.out.print("Enter Number of row or col = ");
+        int n = pat.nextInt();
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=n-i;j++){    // if print triangle then condition is  < j<= i >
+                System.out.print("#"+"  ");
+            }
+            for (int k=1 ;k<=i ;k++){
+                System.out.print((char) (k+64)+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/patterns/triangleVFStar.java
+++ src/patterns/triangleVFStar.java
@@ -1,3 +1,4 @@
+// // SIMPLE METHODS
 //package patterns;
 //
 //import java.util.Scanner;
@@ -20,29 +21,7 @@
 //}
 
 
-
-package patterns;
-
-import java.util.Scanner;
-
-public class triangleVFStar {
-    public static void main(String[] args) {
-        Scanner pat = new Scanner(System.in);
-        System.out.print("Enter Number of row or col = ");
-        int n = pat.nextInt();
-        for (int i =1;i<=n;i++){
-            for (int j =1;j<=(n+1)-(i+1);j++){    // if print triangle then condition is  < j<= i >
-                System.out.print(" "+"  ");
-            }
-            for (int k=1 ;k<=(n+1)-(n-i+1) ;k++){
-                System.out.print("*"+"  ");
-            }
-            System.out.println();
-        }
-    }
-}
-
-//
+// //DOING SELF
 //package patterns;
 //
 //import java.util.Scanner;
@@ -53,13 +32,37 @@ public class triangleVFStar {
 //        System.out.print("Enter Number of row or col = ");
 //        int n = pat.nextInt();
 //        for (int i =1;i<=n;i++){
-//            for (int j =1;j<=n-i;j++){    // if print triangle then condition is  < j<= i >
-//                System.out.print("#"+"  ");
+//            for (int j =1;j<=(n+1)-(i+1);j++){    // if print triangle then condition is  < j<= i >
+//                System.out.print(" "+"  ");
 //            }
-//            for (int k=1 ;k<=i ;k++){
+//            for (int k=1 ;k<=(n+1)-(n-i+1) ;k++){
 //                System.out.print("*"+"  ");
 //            }
 //            System.out.println();
 //        }
 //    }
 //}
+
+
+
+//STANDARD METHODS .
+package patterns;
+
+import java.util.Scanner;
+
+public class triangleVFStar {
+    public static void main(String[] args) {
+        Scanner pat = new Scanner(System.in);
+        System.out.print("Enter Number of row or col = ");
+        int n = pat.nextInt();
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=n-i;j++){    // if print triangle then condition is  < j<= i >
+                System.out.print("#"+"  ");
+            }
+            for (int k=1 ;k<=i ;k++){
+                System.out.print("*"+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
