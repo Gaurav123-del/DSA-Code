e5a5e5f1f85afc8bffabe4e1a3521c586b9633e2
Gaurav123-del <gauravkumarraju1@gmail.com>
2026-03-22 17:50:09 +0530

    Pyramid  Pattern

---
+++ src/patterns/pyramid.java
@@ -0,0 +1,52 @@
+//// METHODS ONE (MATH)
+//package patterns;
+//
+//import java.util.Scanner;
+//
+//public class pyramid {
+//
+//    public static void main(String[] args) {
+//        Scanner pat = new Scanner(System.in);
+//        System.out.print("Enter Number of row or col = ");
+//        int n = pat.nextInt();
+//        for (int i =1;i<=n;i++){
+//            for (int j =1;j<=n-i;j++){    // if print triangle then condition is  < j<= i >
+//                System.out.print(" "+"  ");
+//            }
+//            for (int k=1 ;k<=(i*2)-1 ;k++){
+//                System.out.print("*"+"  ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//}
+
+
+
+// METHODS Second  ( Not MATH)
+package patterns;
+
+import java.util.Scanner;
+
+public class pyramid {
+
+    public static void main(String[] args) {
+        Scanner pat = new Scanner(System.in);
+        System.out.print("Enter Number of row or col = ");
+        int n = pat.nextInt();
+        int nsp =n-1 ,nst=1 ;
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=nsp;j++){    // if print triangle then condition is  < j<= i >
+                System.out.print(" "+"  ");
+            }
+            for (int k=1 ;k<=nst ;k++){
+                System.out.print("*"+"  ");
+            }
+            nsp--;
+            nst += 2;
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/patterns/specialTriangleHW.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class specialTriangleHW {
+
+    public static void main(String[] args) {
+        Scanner pat = new Scanner(System.in);
+        System.out.print("Enter Number of row or col = ");
+        int n = pat.nextInt();
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=i-1;j++){    // if print triangle then condition is  < j<= i >
+                System.out.print(" "+"  ");
+            }
+            for (int k=1 ;k<=(n+1)-i ;k++){
+                System.out.print("*"+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
