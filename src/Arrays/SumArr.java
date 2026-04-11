486284371905fd94c1a3b2144fa59392c681bd84
Gaurav123-del <gauravkumarraju1@gmail.com>
2026-04-11 22:51:01 +0530

    Sum Array Element

---
+++ src/Arrays/SumArr.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class SumArr {
+    public static void main(String[] args) {
+        Scanner ar = new Scanner(System.in);
+        System.out.print("Enter thr length of array: ");
+        int n = ar.nextInt();
+        int[] arr =new int[n];
+        System.out.print("Enter element: ");
+        for (int i=0;i<n;i++) {
+            arr[i] = ar.nextInt();
+        }
+        int sum =0;
+        for ( int i =0; i<n ;i++){
+            sum +=arr[i];
+        }
+        System.out.println(sum);
+    }
+}
